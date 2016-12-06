14a8d75d7d34e5c28b0a80490b7ea524335af47c
Kevin Wolf <kevinw@software-logistics.com>
2016-12-06 07:52:11 -0500

    added startup and devicereposettings

---
+++ src/LagoVista.IoT.DeviceAdmin/Startup.cs
@@ -0,0 +1,18 @@
+﻿using LagoVista.IoT.DeviceAdmin.Interfaces.Managers;
+using LagoVista.IoT.DeviceAdmin.Managers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LagoVista.IoT.DeviceAdmin
+{
+    public class Startup
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IAttributeManager, AttributeManager>();
+        }
+    }
+}
---
+++ src/LagoVista.IoT.Web.DeviceAdmin/Configuration/DeviceRepoSettings.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using LagoVista.Core.Interfaces;
+using LagoVista.IoT.DeviceAdmin.CloudRepos;
+using LagoVista.Core.Models;
+
+namespace LagoVista.IoT.Web.DeviceAdmin
+{
+    public class DeviceRepoSettings : IDeviceRepoSettings
+    {
+        public DeviceRepoSettings(IConfigurationRoot configuration)
+        {
+            var deviceDocDbSection = configuration.GetSection("DeviceDocDbStorage");
+            DeviceDocDbStorage = new ConnectionSettings()
+            {
+                AccessKey = deviceDocDbSection["AccessKey"],
+                Uri = deviceDocDbSection["Endpoint"]  ,
+                ResourceName = deviceDocDbSection["DbName"]
+            };
+
+            var tableStorageSection = configuration.GetSection("DeviceTableStorage");
+            DeviceTableStorage = new ConnectionSettings()
+            {
+                AccountId = tableStorageSection["Name"],
+                AccessKey = tableStorageSection["AccessKey"]                
+            };
+        }
+
+        public IConnectionSettings DeviceDocDbStorage { get; set; }
+
+        public IConnectionSettings DeviceTableStorage { get; set; }
+
+    }
+}
