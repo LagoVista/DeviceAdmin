4e399627ec987be1cba783b9b703e2897a0886a3
Kevin D. Wolf <kevinw@software-logistics.com>
2016-12-20 07:03:24 -0500

    add domain descriptions

--- src/LagoVista.IoT.DeviceAdmin/Models/DeviceAdminDomain.cs
+++ src/LagoVista.IoT.DeviceAdmin/Models/DeviceAdminDomain.cs
@@ -1,4 +1,5 @@
 ﻿using LagoVista.Core.Attributes;
+using LagoVista.Core.Models.UIMetaData;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +8,58 @@ using System.Threading.Tasks;
 namespace LagoVista.IoT.DeviceAdmin.Models
 {
     [DomainDescriptor]
-    public class DeviceAdminDomain
+    public class DeviceAdminDomainMetaData
     {
         //[DomainDescription(Name: "Device Admin", Description: "Models for working with and creating device configurations.  This includes things such as actions, attributes and state machines.")]
         public const string DeviceAdmin = "DeviceAdmin";
 
-      //  [DomainDescription(Name: "State Machines", Description: "State Machines are used for a number of system level components.")]
+        //  [DomainDescription(Name: "State Machines", Description: "State Machines are used for a number of system level components.")]
         public const string StateMachines = "StateMachine";
+
+        [DomainDescription(StateMachines)]
+        public DomainDescription StateMachineDomain
+        {
+            get
+            {
+                return new DomainDescription()
+                {
+                    Description = "A general purpose set of classes to implement data driven inter communicating state machines.",
+                    DomainType = DomainDescription.DomainTypes.BusinessObject,
+                    Name = "State Machine",
+                    CurrentVersion = new Core.Models.VersionInfo()
+                    {
+                        Major = 0,
+                        Minor = 8,
+                        Build = 001,
+                        DateStamp = new DateTime(2016, 12, 20),
+                        Revision = 1,
+                        ReleaseNotes = "Initial unstable preview"
+                    }
+                };
+            }
+        }
+
+        [DomainDescription(DeviceAdmin)]
+        public DomainDescription DeviceAdminDomain
+        {
+            get
+            {
+                return new DomainDescription()
+                {
+                    Description = "A set of classes that contains meta data for managing IoT devices.",
+                    DomainType = DomainDescription.DomainTypes.BusinessObject,
+                    Name = "Device Administration",
+                    CurrentVersion = new Core.Models.VersionInfo()
+                    {
+                        Major = 0,
+                        Minor = 8,
+                        Build = 001,
+                        DateStamp = new DateTime(2016, 12, 20),
+                        Revision = 1,
+                        ReleaseNotes = "Initial unstable preview"
+                    }
+                };
+            }
+        }
     }
 }
