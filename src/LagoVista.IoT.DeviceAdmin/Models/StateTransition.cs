ff2784694738ffcdbe58b86f834f2cb649f6a843
Kevin D. Wolf <kevinw@software-logistics.com>
2017-03-06 17:41:12 -0500

    change entity type on transition

--- src/LagoVista.IoT.DeviceAdmin/Models/StateTransition.cs
+++ src/LagoVista.IoT.DeviceAdmin/Models/StateTransition.cs
@@ -1,5 +1,6 @@
 ﻿using LagoVista.Core.Attributes;
 using LagoVista.Core.Interfaces;
+using LagoVista.Core.Models;
 using LagoVista.IoT.DeviceAdmin.Resources;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -10,9 +11,9 @@ namespace LagoVista.IoT.DeviceAdmin.Models
     public class StateTransition
     {
         [FormField(LabelResource: Resources.DeviceLibraryResources.Names.StateMachine_Event, HelpResource: DeviceLibraryResources.Names.StateMachine_Transition_EventHelp, FieldType: FieldTypes.ChildItem, ResourceType: typeof(DeviceLibraryResources))]
-        public IEntityHeader Event { get; set; }
+        public EntityHeader<Event> Event { get; set; }
         [FormField(LabelResource: Resources.DeviceLibraryResources.Names.StateMachine_NewState, HelpResource: DeviceLibraryResources.Names.StateMachine_NewState_Help, FieldType: FieldTypes.ChildItem, ResourceType: typeof(DeviceLibraryResources))]
-        public IEntityHeader NewState { get; set; }
+        public EntityHeader<State> NewState { get; set; }
 
         [FormField(LabelResource: Resources.DeviceLibraryResources.Names.StateMachine_Transition_Script, HelpResource: DeviceLibraryResources.Names.StateMachine_Transition_Action_Help, FieldType: FieldTypes.NodeScript, ResourceType: typeof(DeviceLibraryResources))]
         public string TransitionAction { get; set; }
