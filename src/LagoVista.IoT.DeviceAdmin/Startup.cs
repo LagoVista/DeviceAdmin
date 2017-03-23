e623dd73792b2c87969bf672cfcb5838c4297f28
Kevin D. Wolf <kevindwolf@hotmail.com>
2017-03-23 18:11:38 -0400

    compiling w/ VS2017

--- src/LagoVista.IoT.DeviceAdmin/Startup.cs
+++ src/LagoVista.IoT.DeviceAdmin/Startup.cs
@@ -1,10 +1,6 @@
 ﻿using LagoVista.IoT.DeviceAdmin.Interfaces.Managers;
 using LagoVista.IoT.DeviceAdmin.Managers;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LagoVista.IoT.DeviceAdmin
 {
