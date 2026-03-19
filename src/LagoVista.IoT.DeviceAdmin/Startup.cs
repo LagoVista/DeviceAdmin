235caa40fda735cd6d9c595ce91da9b15467375d
Kevin Wolf <kevinw@software-logistics.com>
2026-03-19 17:57:41 -0400

    add new DI registration strategy

--- src/LagoVista.IoT.DeviceAdmin/Startup.cs
+++ src/LagoVista.IoT.DeviceAdmin/Startup.cs
@@ -2,10 +2,10 @@
 // ContentHash: 530759fdd08f3a6077fb937e9ea42e1bc2cd6a0994fa8fd7342da0c6b44e147b
 // IndexVersion: 2
 // --- END CODE INDEX META ---
-using LagoVista.Core.Interfaces;
 using LagoVista.IoT.DeviceAdmin.Interfaces.Managers;
 using LagoVista.IoT.DeviceAdmin.Managers;
 using LagoVista.IoT.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using System.Resources;
 
 [assembly: NeutralResourcesLanguage("en")]
